ad47c867437da4a2927c1f63ff64e225bca22bb8
zhuxudong <callzhuxudong@163.com>
2023-04-27 15:57:04 +0800

    fix: bug test

--- playground/ark-sdk.ts
+++ playground/ark-sdk.ts
@@ -2,17 +2,49 @@
  * @title Ark SDK
  * @category Animation
  */
-import { AssetType, Camera, Loader, Logger, Scene, WebGLEngine } from "@galacean/engine";
-import { OrbitControl } from "@galacean/engine-toolkit";
+import {
+  Animator,
+  AssetType,
+  Camera,
+  Loader,
+  Logger,
+  Scene,
+  StateMachineScript,
+  WebGLEngine,
+  WrapMode
+} from "@galacean/engine";
 import { ArkComponent, Mouth, screenshotSchema } from "@galacean/engine-ark";
+import { OrbitControl } from "@galacean/engine-toolkit";
 import * as dat from "dat.gui";
 Loader.registerClass("ArkComponent", ArkComponent);
 Loader.registerClass("TTSAComponent", ArkComponent);
 const gui = new dat.GUI();
 Logger.enable();
 
+// screenshotSchema(
+//   "https://mdn.alipayobjects.com/oasis_be/afts/file/A*e1QmRqCAgFsAAAAAAAAAAAAADkp5AQ/scene.json",
+//   1024,
+//   1024
+// ).then((blob) => {
+//   const url = window.URL.createObjectURL(blob);
+//   window.open(url);
+// });
+
+// WebGLEngine.create({ canvas: "canvas" }).then((engine) => {
+//   init(engine);
+// });
+const crossFadeDuration = 0.1;
+
+class IdleScript extends StateMachineScript {
+  async onStateExit(animator: Animator) {
+    animator.crossFade("idle", crossFadeDuration);
+    this.destroy();
+  }
+}
+
 const engine = new WebGLEngine("canvas", { alpha: true });
 init(engine);
+
 function init(engine: WebGLEngine) {
   engine.canvas.resizeByClientSize();
   engine.resourceManager
@@ -23,6 +55,7 @@ function init(engine: WebGLEngine) {
     .then(async (scene) => {
       engine.sceneManager.activeScene = scene;
       engine.run();
+
       // return;
       const root = scene.getRootEntity(4);
       // @ts-ignore
@@ -34,7 +67,7 @@ function init(engine: WebGLEngine) {
         scene.getRootEntity(i)?.getComponentsIncludeChildren(Camera, cameras);
         if (cameras.length) break;
       }
-      cameras[0].entity.addComponent(OrbitControl).target.set(0, 1, 0);
+      // cameras[0].entity.addComponent(OrbitControl).target.set(0, 1, 0);
       // SDK
       await Promise.all([
         ark.addMouth("/Assets/Char/Female/Model/ANI/KX/mouth_A", Mouth.A),
@@ -45,34 +78,46 @@ function init(engine: WebGLEngine) {
         ark.addAnimation("/Assets/Char/Female/Model/ANI/DZ/idle", "idle")
       ]);
 
-      // const { layerIndex, state } = await ark.addAnimation("/Assets/Char/Female/Model/ANI/DZ/ANI_dz_gx.fbx");
-      // /Assets/Char/Female/Model/ANI/DZ/ANI_dz_gx.fbx
-      // ark.animator.play(state.name, layerIndex);
       ark.animator.play("idle");
-      const debug = {
-        A: 0,
-        E: 0,
-        I: 0,
-        O: 0,
-        U: 0
-      };
-
-      gui.add(debug, "A", 0, 1, 0.01).onChange((value) => {
-        ark.playMouth(Mouth.A, value);
-      });
-      gui.add(debug, "E", 0, 1, 0.01).onChange((value) => {
-        ark.playMouth(Mouth.E, value);
-      });
-      gui.add(debug, "I", 0, 1, 0.01).onChange((value) => {
-        ark.playMouth(Mouth.I, value);
-      });
-      gui.add(debug, "O", 0, 1, 0.01).onChange((value) => {
-        ark.playMouth(Mouth.O, value);
-      });
-      gui.add(debug, "U", 0, 1, 0.01).onChange((value) => {
-        ark.playMouth(Mouth.U, value);
-      });
-
       engine.run();
+
+      driveAction(ark, "/Assets/Char/Female/Model/ANI/DZ/ANI_ysszzysf_ph.fbx");
+      setTimeout(() => {
+        driveAction(ark, "/Assets/Char/Female/Model/ANI/DZ/ANI_ysszzysf_ph.fbx");
+      }, 1000);
+      // const debug = {
+      //   A: 0,
+      //   E: 0,
+      //   I: 0,
+      //   O: 0,
+      //   U: 0
+      // };
+
+      // gui.add(debug, "A", 0, 1, 0.01).onChange((value) => {
+      //   ark.playMouth(Mouth.A, value);
+      // });
+      // gui.add(debug, "E", 0, 1, 0.01).onChange((value) => {
+      //   ark.playMouth(Mouth.E, value);
+      // });
+      // gui.add(debug, "I", 0, 1, 0.01).onChange((value) => {
+      //   ark.playMouth(Mouth.I, value);
+      // });
+      // gui.add(debug, "O", 0, 1, 0.01).onChange((value) => {
+      //   ark.playMouth(Mouth.O, value);
+      // });
+      // gui.add(debug, "U", 0, 1, 0.01).onChange((value) => {
+      //   ark.playMouth(Mouth.U, value);
+      // });
     });
+}
+
+async function driveAction(ark: ArkComponent, url: string) {
+  const { layerIndex, state } = await ark.addAnimation(url, undefined, WrapMode.Once);
+  const animator = ark.animator;
+
+  // @ts-ignore
+  state._onStateExitScripts.length = 0;
+
+  animator.crossFade(state.name, crossFadeDuration, layerIndex);
+  state.addStateMachineScript(IdleScript);
 }
--- playground/fix.ts
+++ playground/fix.ts
@@ -5,83 +5,52 @@
 import {
   AmbientLight,
   AssetType,
-  BoundingBox,
   Camera,
   GLTFResource,
   Logger,
-  MeshRenderer,
-  PrimitiveMesh,
-  UnlitMaterial,
-  Vector3,
-  WebGLEngine
+  WebGLEngine,
+  PBRMaterial,
+  RenderFace
 } from "@galacean/engine";
-import { OrbitControl, WireframeManager, WireframeManager } from "@galacean/engine-toolkit";
 Logger.enable();
-const engine = new WebGLEngine("canvas", {
-  antialias: true,
-  alpha: true
+
+WebGLEngine.create({ canvas: "canvas" }).then((engine) => {
+  engine.canvas.resizeByClientSize();
+  const scene = engine.sceneManager.activeScene;
+  // scene.background.solidColor.set(0, 0, 0, 0);
+  engine.run();
+
+  // root
+  const root = scene.createRootEntity();
+
+  // camera
+  const cameraEntity = root.createChild("camera");
+  cameraEntity.addComponent(Camera);
+  // cameraEntity.addComponent(OrbitControl).target.set(0, 0, 0);
+  cameraEntity.transform.position.set(0, 1, 3);
+
+  // env
+  engine.resourceManager
+    .load<AmbientLight>({
+      type: AssetType.Env,
+      url: "https://gw.alipayobjects.com/os/bmw-prod/67b05052-ecf8-46f1-86ff-26d9abcc83ea.bin"
+    })
+    .then((ambientLight) => {
+      ambientLight.diffuseIntensity = 0.5;
+      ambientLight.specularIntensity = 0.5;
+      scene.ambientLight = ambientLight;
+    });
+
+  // loadModel
+  engine.resourceManager
+    .load<GLTFResource>("https://gw.alipayobjects.com/os/H5App-BJ/1669174661711-gltf/female_character/scene/scene.gltf")
+    .then((resource) => {
+      root.addChild(resource.defaultSceneRoot);
+      const materials = resource.materials;
+      materials?.forEach((m) => {
+        // m.renderFace = RenderFace.Front;
+        // m.isTransparent = true;
+        // m.baseColor.a = 0.5;
+      });
+    });
 });
-// engine
-engine.canvas.resizeByClientSize();
-const scene = engine.sceneManager.activeScene;
-// scene.background.solidColor.set(0, 0, 0, 0);
-engine.run();
-
-// root
-const root = scene.createRootEntity();
-
-// camera
-const cameraEntity = root.createChild("camera");
-cameraEntity.addComponent(Camera);
-cameraEntity.addComponent(OrbitControl).target.set(0, 0, 0);
-cameraEntity.transform.position.set(0, 0, 1);
-
-// env
-engine.resourceManager
-  .load<AmbientLight>({
-    type: AssetType.Env,
-    url: "https://gw.alipayobjects.com/os/bmw-prod/67b05052-ecf8-46f1-86ff-26d9abcc83ea.bin"
-  })
-  .then((ambientLight) => {
-    ambientLight.diffuseIntensity = 0.5;
-    ambientLight.specularIntensity = 0.5;
-    scene.ambientLight = ambientLight;
-  });
-
-const wireframeManager = root.addComponent(WireframeManager);
-// loadModel
-engine.resourceManager
-  .load<GLTFResource>({
-    assetType: AssetType.Prefab,
-    url: "https://mdn.alipayobjects.com/chain_myent/afts/file/ew1MRoLexJoAAAAAAAAAAAAADvN2AQBr#.gltf"
-  })
-  .then((resource) => {
-    const { defaultSceneRoot, entities } = resource;
-
-    root.addChild(defaultSceneRoot);
-    console.log(resource);
-
-    const renderers = defaultSceneRoot.getComponentsIncludeChildren(MeshRenderer, []);
-    const boundingBox = renderers[0].bounds.clone();
-    console.log(renderers);
-    for (let i = renderers.length - 1; i > 0; i--) {
-      const mesh = renderers[i].mesh;
-      // if (mesh.name === "Bound") {
-      // continue;
-      // }
-      BoundingBox.merge(boundingBox, renderers[i].bounds, boundingBox);
-      wireframeManager.addRendererWireframe(renderers[i]);
-    }
-
-    const extent = new Vector3();
-    boundingBox.getExtent(extent);
-    console.log(boundingBox);
-    const entity = root.createChild();
-    const renderer = entity.addComponent(MeshRenderer);
-    renderer.mesh = PrimitiveMesh.createCuboid(engine, extent.x * 2, extent.y * 2, extent.z * 2);
-    const material = new UnlitMaterial(engine);
-    material.isTransparent = true;
-    material.baseColor.a = 0.5;
-    renderer.setMaterial(material);
-    console.log(extent);
-  });
